d635501a68e39f203b5e15203da61b396c7cd281
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-08-22 15:46:30 -0400

    Setup & Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+      },
+    },
   },
   plugins: [],
-}
+};
